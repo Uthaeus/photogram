64c1d400937bf8fdc28c661ecf372a8d9e0a8720
Uthaeus <romanlavery@gmail.com>
2018-07-19 18:37:20 -0600

    Adjusted comments controller to user ajax and implemented js to render new comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,9 +6,10 @@ class CommentsController < ApplicationController
     @comment.user_id = current_user.id 
 
     if @comment.save
-      flash[:success] = "Nice comment"
-
-      redirect_to :back
+      respond_to do |format| 
+        format.html { redirect_to root_path }
+        format.js
+      end
     else
       flash[:alert] = "Something went wrong, check comment form"
       render root_path
