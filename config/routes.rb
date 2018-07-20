538745a071b5c752cf9aa2d68e733b3fa943325a
Uthaeus <romanlavery@gmail.com>
2018-07-20 14:10:59 -0600

    Created controller for profiles, styled devise edit page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'profiles/show'
+
   devise_for :users, :controllers => { registrations: 'registrations' }
 
   root to: 'posts#index'
@@ -7,5 +9,7 @@ Rails.application.routes.draw do
   resources :posts do 
     resources :comments
   end
+
+  get ':user_name', to: 'profiles#show', as: :profile 
 
 end
