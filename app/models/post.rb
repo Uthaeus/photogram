b7e2cee7f5ad20e06375cd048843200b85aa962f
Uthaeus <romanlavery@gmail.com>
2018-07-19 18:15:17 -0600

    Generated model and controller for comments, added comment form to posts layout

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,6 @@ class Post < ApplicationRecord
   has_attached_file :image, styles: { :medium => "640x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
+  has_many :comments, dependent: :destroy
+
 end
